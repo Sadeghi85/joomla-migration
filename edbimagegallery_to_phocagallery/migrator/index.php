be5e17f097e87a3207c29e7aa4a747215471e403
Mohammad Sadeghi <sadeghi85@hotmail.com>
2013-10-19 14:17:25 +0330

    .

--- edbimagegallery_to_phocagallery/migrator/index.php
+++ edbimagegallery_to_phocagallery/migrator/index.php
@@ -7,6 +7,12 @@ $joom_3_user = 'chehreha3admin';
 $joom_3_pass = 'chehreha3admin';
 $joom_3_dbprefix = 'n5lfb_';
 
+$media_address = 'http://media.iribtv.ir/Images/ImageGallery/';
+$media_local_address = __DIR__.'/../images/phocagallery/old_gallery/';
+$media_local_address_relative = 'old_gallery/';
+
+@mkdir($media_local_address, '0755', true);
+
 $joom_3_category_array = array();
 
 // Config for old Joomla 1.0 database
@@ -26,63 +32,58 @@ try {
 	// INNER JOIN xref_image_gallery ON `tbl_image`.`image_code` = `xref_image_gallery`.`image_code`
 	// WHERE `tbl_image`.`s_site_code` = 'SC16000000'
 	
+	// SELECT * FROM `tbl_image_gallery`
+	// WHERE `s_site_code` = 'SC16000000'
+		// AND `ig_parentcode` = 'igc0000'
+	
+	
+	## 1. Creating first level parent category
+	$sth_phoca_category = $dbh_joom_3->prepare(sprintf('INSERT INTO `%s` (title,alias,published,approved,access,language) VALUES (:title,:alias,:published,:approved,:access,:language)', $joom_3_dbprefix.'phocagallery_categories'));
+	
+	$sth_phoca_category->execute(array(':title'=>'سرشاخه گالری قدیمی',':alias'=>str_replace(' ', '-', 'سرشاخه گالری قدیمی'),':published'=>'1',':approved'=>'1',':access'=>'1',':language'=>'*'));
+	// 'سرشاخه گالری قدیمی' : 'igc0000'
+	$joom_3_category_array['igc0000'] = $dbh_joom_3->lastInsertId();
+	## /Creating first level parent category
+	
+	## 2. Creating the rest of categories
+	$sth_image_gallery = $dbh_joom_1->prepare(sprintf("SELECT * FROM `%s` WHERE `%s` = '%s' ORDER BY CAST(SUBSTRING(`%s`, 4) AS UNSIGNED)", 'tbl_image_gallery', 's_site_code', $s_site_code, 'ig_code'));// LIMIT 0, 20');
+	$sth_image_gallery->execute();
+	
+	$sth_phoca_category = $dbh_joom_3->prepare(sprintf('INSERT INTO `%s` (parent_id,title,alias,published,approved,access,language) VALUES (:parent_id,:title,:alias,:published,:approved,:access,:language)', $joom_3_dbprefix.'phocagallery_categories'));
+	
+	while ($row = $sth_image_gallery->fetch(PDO::FETCH_ASSOC))
+	{
+		if ( ! array_key_exists($row['ig_code'], $joom_3_category_array))
+		{
+			$sth_phoca_category->execute(array(':parent_id'=>$joom_3_category_array[$row['ig_parentcode']],':title'=>$row['ig_title'],':alias'=>str_replace(' ', '-', $row['ig_title']),':published'=>'1',':approved'=>'1',':access'=>'1',':language'=>'*'));
+			
+			$joom_3_category_array[$row['ig_code']] = $dbh_joom_3->lastInsertId();
+		}
+    }
+	## /Creating the rest of categories
+	
+	
+	## 3. Importing images
 	$sth_images = $dbh_joom_1->prepare(sprintf('SELECT * FROM `%s` INNER JOIN `%s` ON `%s`.`%s` = `%s`.`%s` WHERE `%s`.`%s` = \'%s\'', 'tbl_image', 'xref_image_gallery', 'tbl_image', 'image_code', 'xref_image_gallery', 'image_code', 'tbl_image', 's_site_code', $s_site_code));// LIMIT 0, 20');
 	$sth_images->execute();
 	
 	$sth_phoca_image = $dbh_joom_3->prepare(sprintf('INSERT INTO `%s` (catid,title,alias,filename,format,description,date,hits,published,approved,language) VALUES (:catid,:title,:alias,:filename,:format,:description,:date,:hits,:published,:approved,:language)', $joom_3_dbprefix.'phocagallery'));
 	
-	$sth_phoca_category = $dbh_joom_3->prepare(sprintf('INSERT INTO `%s` (title,alias,published,approved,access,language) VALUES (:title,:alias,:published,:approved,:access,:language)', $joom_3_dbprefix.'phocagallery_categories'));
-	
 	while ($row = $sth_images->fetch(PDO::FETCH_ASSOC))
 	{
-		if ( ! array_key_exists($row['ig_code'], $joom_3_category_array))
-		{
-			$sth_phoca_category->execute(array(':title'=>$row['ig_code'],':alias'=>$row['ig_code'],':published'=>'1',':approved'=>'1',':access'=>'1',':language'=>'*'));
-			
-			$joom_3_category_array[$row['ig_code']] = $dbh_joom_3->lastInsertId();
-		}
-	
-		$sth_phoca_image->execute(array(':catid'=>$joom_3_category_array[$row['ig_code']],':title'=>$row['image_title'],':alias'=>str_replace(' ','-',$row['image_title']),':filename'=>'1',':format'=>'1',':description'=>$row['image_comment'],':date'=>$row['modified'],':hits'=>$row['image_hit'],':published'=>'1',':approved'=>'1',':language'=>'*'));
+		@mkdir($media_local_address.$joom_3_category_array[$row['ig_code']], '0755');
+		
+		$sth_phoca_image->execute(array(':catid'=>$joom_3_category_array[$row['ig_code']],':title'=>$row['image_title'],':alias'=>str_replace(' ','-',$row['image_title']),':filename'=>$media_local_address_relative.$joom_3_category_array[$row['ig_code']].'/'.$row['image_code'].'.jpg',':format'=>'1',':description'=>$row['image_comment'],':date'=>$row['modified'],':hits'=>$row['image_hit'],':published'=>'1',':approved'=>'1',':language'=>'*'));
+		
+		file_put_contents($media_local_address.$joom_3_category_array[$row['ig_code']].'/'.$row['image_code'].'.jpg', file_get_contents($media_address.$row['image_name']));
     }
+	## /Importing images
 	
-	
-	// ########################## images
-	// $sth_images = $dbh_joom_1->prepare(sprintf('SELECT * FROM `%s` WHERE `%s` = \'%s\'', 'tbl_image', 's_site_code', $s_site_code));// LIMIT 0, 20');
-	// $sth_images->execute();
-	
-	// while ($row = $sth_images->fetch(PDO::FETCH_ASSOC))
-	// {
 		
-    // }
-	
-	
-	// ########################## categories
-	// $sth_categories = $dbh_joom_1->prepare(sprintf('SELECT * FROM `%s` WHERE `%s` = \'%s\'', 'tbl_image_gallery', 's_site_code', $s_site_code));// LIMIT 0, 20');
-	// $sth_categories->execute();
-	
-	// while ($row = $sth_categories->fetch(PDO::FETCH_ASSOC))
-	// {
-		
-    // }
-	
-// SELECT * FROM `xref_image_gallery`
-// WHERE `xref_image_gallery`.`image_code` IN 
-	// (SELECT `tbl_image`.`image_code` FROM `tbl_image` WHERE `tbl_image`.`s_site_code` = 'SC16000000')
-	
-	// ########################## category_image
-	// $sth_category_image = $dbh_joom_1->prepare(sprintf('SELECT * FROM `%s` WHERE `%s`.`%s` IN (SELECT `%s`.`%s` FROM `%s` WHERE `%s`.`%s` = \'%s\')', 'xref_image_gallery', 'xref_image_gallery', 'image_code', 'tbl_image', 'image_code', 'tbl_image', 'tbl_image', 's_site_code', $s_site_code));// LIMIT 0, 20');
-	// $sth_category_image->execute();
-	
-	// while ($row = $sth_category_image->fetch(PDO::FETCH_ASSOC))
-	// {
-		
-    // }
-	
-	
     $dbh_joom_1 = null;
 	$dbh_joom_3 = null;
 }
-catch (PDOException $e)
+catch (Exception $e)
 {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
